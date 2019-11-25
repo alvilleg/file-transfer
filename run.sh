f034fedfda59e22a1909a9491f01bd6e3a2771b2
aldemarvillegas@seven4n.com <aldemarvillegas@seven4n.com>
2019-11-25 18:17:47 -0500

    Fixed bugs

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #/bin/bash
+month=16 #$(date +"%_m")
+day=$(date +"%_d")
+year=$(date +"%Y")
+echo $day-$month-$year
 java \
--DhostnameTarget=181.55.123.197 \
+-DhostnameTarget=192.168.0.200 \
 -DpasswordTarget=@Simple \
 -DusernameTarget=ftpsimple \
--DhostnameSource=localhost -DpasswordSource=alde1234 \
--DusernameSource=alde  \
--DfullPathToDownload=/Users/alde/Downloads/89_CCF57_201911_S_N_I_2962853.zip \
--DlocalTargetPath=/tmp/  \
--DremoteTargetPath=/Users/alde/Documents/work/alianzap/ \
+-DhostnameSource=localhost -DpasswordSource=Absolute4n \
+-DusernameSource=aldemarv  \
+-DfullPathToDownload=/home/aldemarv/Downloads/publicaciones/comfandiftp/$year/$month/$day/CCF \
+-DlocalTargetPath=/tmp/file-transfer  \
+-DremoteTargetPath=/Cuota_mon/PROCESS/ReAp_Pil/Pend_Inb/Opr_Info/ \
 -DlocalFileName=89_CCF57_201911_S_N_I_2962853.zip  \
--classpath :target/simple-file-transfer-1.0-SNAPSHOT-jar-with-dependencies.jar:lib/bcprov-jdk15on-1.60.jar:lib/bcpkix-jdk15on-1.60.jar:lib/bcprov-jdk15on-1.60.jar:lib/eddsa-0.2.0.jar:lib/jzlib-1.1.3.jar:lib/slf4j-api-1.7.7.jar:lib/sshj-0.27.0.jar:lib/deploy.jar:lib/ext/cldrdata.jar:lib/ext/dnsns.jar:lib/ext/jaccess.jar:lib/ext/jfxrt.jar:lib/ext/localedata.jar:lib/ext/nashorn.jar:lib/ext/sunec.jar:lib/ext/sunjce_provider.jar:lib/ext/sunpkcs11.jar:lib/ext/zipfs.jar:lib/javaws.jar:lib/jce.jar:lib/jfr.jar:lib/jfxswt.jar:lib/jsse.jar:lib/management-agent.jar:lib/plugin.jar:lib/resources.jar:lib/rt.jar:ant-javafx.jar:dt.jar:javafx-mx.jar:jconsole.jar:packager.jar:sa-jdi.jar:tools.jar:lib/commons-net-3.6.jar:lib/commons-io-2.5.jar as.App
+-classpath :target/simple-file-transfer-1.0-SNAPSHOT-jar-with-dependencies.jar as.App
+#:lib/bcprov-jdk15on-1.60.jar:lib/bcpkix-jdk15on-1.60.jar:lib/bcprov-jdk15on-1.60.jar:lib/eddsa-0.2.0.jar:lib/jzlib-1.1.3.jar:lib/slf4j-api-1.7.7.jar:lib/sshj-0.27.0.jar:lib/deploy.jar:lib/ext/cldrdata.jar:lib/ext/dnsns.jar:lib/ext/jaccess.jar:lib/ext/jfxrt.jar:lib/ext/localedata.jar:lib/ext/nashorn.jar:lib/ext/sunec.jar:lib/ext/sunjce_provider.jar:lib/ext/sunpkcs11.jar:lib/ext/zipfs.jar:lib/javaws.jar:lib/jce.jar:lib/jfr.jar:lib/jfxswt.jar:lib/jsse.jar:lib/management-agent.jar:lib/plugin.jar:lib/resources.jar:lib/rt.jar:ant-javafx.jar:dt.jar:javafx-mx.jar:jconsole.jar:packager.jar:sa-jdi.jar:tools.jar:lib/commons-net-3.6.jar:lib/commons-io-2.5.jar
+
--- src/main/java/as/App.java
+++ src/main/java/as/App.java
@@ -10,7 +10,7 @@ import org.apache.commons.net.ftp.FTPReply;
 
 
 import java.io.*;
-import java.util.List;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -87,8 +87,12 @@ public class App {
         app.setRemoteTargetPath(System.getProperty("remoteTargetPath"));
         app.setLocalFileName(System.getProperty("localFileName"));
 
-        app.downloadFile(app.fullPathToDownload, app.localFileName);
-        app.unzip(app.localTargetPath + app.localFileName);
+        try {
+            app.downloadFile(app.fullPathToDownload, app.localFileName);
+        } catch (Exception exc) {
+            System.out.println("Failure reading files from: " + app.fullPathToDownload);
+            exc.printStackTrace();
+        }
 
         System.exit(0);
     }
@@ -96,12 +100,20 @@ public class App {
     public void unzip(String zipFilePath) throws Exception {
         String fileZip = zipFilePath;
         File destDir = new File(localTargetPath);
+        destDir.mkdirs();
         byte[] buffer = new byte[1024];
         ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
         ZipEntry zipEntry = zis.getNextEntry();
+        List<String> order = Arrays.asList("I", "IR", "IP", "IPR", "AP", "APR", "UN", "UR", "A", "AR");
+        Map<String, List<String>> filePathsByType = new LinkedHashMap<>();
+        order.stream().forEach((s) -> filePathsByType.put(s, new ArrayList<>()));
+
         while (zipEntry != null) {
 
             File newFile = new File(destDir, zipEntry.getName());
+
+            System.out.println("Zip entry Name : " + zipEntry.getName());
+
             FileOutputStream fos = new FileOutputStream(newFile);
             int len;
             while ((len = zis.read(buffer)) > 0) {
@@ -109,25 +121,64 @@ public class App {
             }
             System.out.println("Zip entry: " + zipEntry.getName());
             fos.close();
-            uploadFTP(newFile.getCanonicalPath(), zipEntry.getName());
             zipEntry = zis.getNextEntry();
+            String fileName = newFile.getName();
+            String priority = fileName.split("_")[7];
+            List<String> files = filePathsByType.get(priority);
+            files.add(newFile.getCanonicalPath());
+            filePathsByType.put(priority, files);
         }
+
+
+        for (String s : order) {
+            System.out.println("Sending priority type: " + s);
+            for (String filePath : filePathsByType.get(s)) {
+                try {
+                    uploadFTP(filePath);
+                } catch (Exception exc) {
+                    System.out.println("Fail uploading ... " + filePath);
+                    exc.printStackTrace();
+                }
+            }
+            Thread.sleep(1000);
+            filePathsByType.remove(s);
+        }
+
+        for (Map.Entry<String, List<String>> missing : filePathsByType.entrySet()) {
+            for (String filePath : missing.getValue()) {
+                uploadFTP(filePath);
+            }
+        }
+
         zis.closeEntry();
         zis.close();
     }
 
-    public void downloadFile(String fileToDownload, String fileName)
-        throws IOException {
+    public void downloadFile(String fileToDownload, String fileName) throws IOException {
         SSHClient sshClient = setupSshj();
         SFTPClient sftpClient = sshClient.newSFTPClient();
-        sftpClient.get(fileToDownload, localTargetPath + fileName);
+        List<RemoteResourceInfo> remoteResourceInfos = sftpClient.ls(fileToDownload);
+        remoteResourceInfos.stream().filter((s) -> s.getName().contains(".zip")).forEach(rsi -> {
+            System.out.println("Downloading..." + rsi.getPath());
 
+            try {
+                sftpClient.get(rsi.getPath(), localTargetPath + rsi.getName());
+
+                unzip(localTargetPath + rsi.getName());
+
+            } catch (IOException e) {
+                System.out.println("Fail downloading: " + rsi.getPath());
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println("Fail downloading: " + rsi.getPath());
+                e.printStackTrace();
+            }
+        });
         sftpClient.close();
         sshClient.disconnect();
     }
 
-    public void upload(String fullFilePath, String fileName)
-        throws IOException {
+    public void upload(String fullFilePath, String fileName) throws IOException {
         SSHClient sshClient = setupSshj();
         SFTPClient sftpClient = sshClient.newSFTPClient();
 
@@ -137,8 +188,8 @@ public class App {
         sshClient.disconnect();
     }
 
-    public void uploadFTP(String fullFilePath, String fileName)
-        throws Exception {
+    public void uploadFTP(String fullFilePath) throws Exception {
+        System.out.println("Start uploading for " + fullFilePath);
         FTPClient ftp = new FTPClient();
         ftp.connect(hostnameTarget, 21);
         int reply = ftp.getReplyCode();
@@ -151,19 +202,20 @@ public class App {
         if (!logged) {
             throw new Exception("Couldn't log ");
         }
+
         ftp.enterLocalPassiveMode();
         ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
         File file = new File(fullFilePath);
         InputStream in = new FileInputStream(file);
-        OutputStream outputStream = ftp.storeFileStream(file.getName());
-        System.out.println("Sending: ..." + file.getName());
+        OutputStream outputStream = ftp.storeFileStream(remoteTargetPath + file.getName());
+        System.out.println("Sending: ... fullPath: " + fullFilePath + " Name: " + file.getName());
         IOUtils.copy(in, outputStream);
         IOUtils.closeQuietly(in);
         IOUtils.closeQuietly(outputStream);
 
         ftp.logout();
         ftp.disconnect();
-        System.out.println("Finished " + fullFilePath);
+        System.out.println("Success upload for: " + fullFilePath);
     }
 
     private SSHClient setupSshj() throws IOException {
